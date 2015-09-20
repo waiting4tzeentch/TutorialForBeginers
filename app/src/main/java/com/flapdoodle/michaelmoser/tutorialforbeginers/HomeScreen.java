0eba7e8ccadf3542b23c2d77208536bbf9575129
Moser <michael.opper.moser@gmail.com>
2015-09-20 12:20:34 -0400

    Added images
    Images added to drawable and layout.

--- app/src/main/java/com/flapdoodle/michaelmoser/tutorialforbeginers/HomeScreen.java
+++ app/src/main/java/com/flapdoodle/michaelmoser/tutorialforbeginers/HomeScreen.java
@@ -3,11 +3,14 @@ package com.flapdoodle.michaelmoser.tutorialforbeginers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity implements View.OnClickListener {
 
     TextView mainTextView;
+    Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,12 @@ public class HomeScreen extends AppCompatActivity {
 
         mainTextView = (TextView) findViewById(R.id.main_textview);
         mainTextView.setText("Set in Java!");
+
+        // 2. Access the Button defined in layout XML
+        // and listen for it here
+
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
     }
 
     @Override
@@ -26,6 +35,12 @@ public class HomeScreen extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home_screen, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // test the button
+        mainTextView.setText("Button Pressed!");
     }
 
     // removed public boolean onOptionsItemSelected on instructions from tutorial
